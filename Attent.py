f93c2ff7cf9bd02dd32918ab63293c5254fbca30
LeilaGuennif <117037003+LeilaGuennif@users.noreply.github.com>
2022-12-01 20:55:36 +0100

    Create Attent.py

---
+++ Attent.py
@@ -0,0 +1,19 @@
+class attention_trad (Layer) :
+    def __init__(self, enc_outdim, dec_hdim) :    #We need the hidden state of the decoder and the encoder 
+
+        super(attention_trad, self).__init__()
+        
+    def build (self, inp_shape = dec_hdim) : #State of the layer (weights)
+        w_init = tf.keras.initializers.Zeros() 
+        #Weight matrices 
+        self.Va = tf.Variable (initial_value=w_init(shape=(inp_shape[-1], self.units),) trainable=True)#shape : n, with n : number of hidden states 
+        self.Wa = tf.Variable (initial_value = w_init(shape=([inp_shape[-1],inp_shape[-1]],self.units),) trainable=True) #shape : n*n
+        self.Ua = tf.Variable (initial_value = w_init(shape=([inp_shape[-1],2inp_shape[-1]],self.units),)trainable=True) #shape : n*2n
+    def call (self, outp_enc, hidden_dec ) : # hidden state of the decoder, outp_enc = outputs of the encoder
+        #We compute the attention : 
+        a  = self.Va.T*tanh(self.Wa*hidden_dec[-1]+self.Ua*outp_enc )  #hidden_dec[-1] : decoder hidden state of i-1,
+        alpha = tf.nn.softmax(a) #We normalize the output probabilities
+         
+        c = np.sum(alpha*hidden_dec) #context 
+        return c
+ 
