0ec0ad545b6d5303828334f4d36dd752d7c4cfaf
Koloina RANDRIANAVONY <91273793+ko-randria@users.noreply.github.com>
2022-12-01 11:30:41 +0100

    Update Encoder.py

--- Encoder.py
+++ Encoder.py
@@ -2,16 +2,18 @@ from tensorflow.keras.layers import Dense , Input , SimpleRNN, LSTM , Embedding,
 from tensorflow.keras.layers import Bidirectional , Attention, Layer
 #from tensorflow.keras.activations import tanh 
 from tensorflow.keras.activations import *
-class encoder (Layer):
-     def _init_(self, embdim, intdim, enc_dimh, dec_dim_h, t_drop):
-        
-             super(encoder, self)._init_()
-             #ri :output of the reset gate
+import tensorflow as tf
 
-             self.emb = Embedding(intdim, embdim) #size of the sequence, and dimension of the embedding
-             self.RNN =Bidirectional(GRU (enc_dimh)) #Choose the GRU because he returns the hidden states and the outputs 
-             self.drop = Dropout(t_drop)
-             self.lin = Dense (dec_dim_h, activation = None ) #output size,  because Multilayer perceptron 
+class encoder (Layer):
+     
+     def _init_(self, embdim, intdim, enc_dimh, dec_dim_h, t_drop):
+         super(encoder, self)._init_()
+         #ri :output of the reset gate
+
+         self.emb = Embedding(intdim, embdim) #size of the sequence, and dimension of the embedding
+         self.RNN =Bidirectional(GRU (enc_dimh)) #Choose the GRU because he returns the hidden states and the outputs 
+         self.drop = Dropout(t_drop)
+         self.lin = Dense (dec_dim_h, activation = None ) #output size,  because Multilayer perceptron 
              
      def call (self, seq) :
          emb_mod  = self.emb(seq) #Embedding step we give to our data different dimension 
