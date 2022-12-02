2e97c6a1fab32079e5ebd7a87f1da4e0b8fe4a41
Koloina RANDRIANAVONY <91273793+ko-randria@users.noreply.github.com>
2022-12-02 02:04:45 +0100

    Update Attention.py

--- Attention.py
+++ Attention.py
@@ -1,20 +1,29 @@
-class attention_trad (Layer) :
+from tensorflow.keras.layers import Layer
+from tensorflow.keras.activations import *
+import tensorflow as tf
+import numpy as np 
+
+class Attention_trad (Layer) :
     def __init__(self, enc_outdim, dec_hdim) :    #We need the hidden state of the decoder and the encoder 
 
-        super(attention_trad, self).__init__()
+        super(Attention_trad, self).__init__()
         
-    def build (self, inp_shape = dec_hdim) : #State of the layer (weights)
-        w_init = tf.keras.initializers.Zeros() 
-        #Weight matrices 
-        self.Va = tf.Variable (initial_value=w_init(shape=(inp_shape[-1], self.units),) trainable=True)#shape : n, with n : number of hidden states 
-        self.Wa = tf.Variable (initial_value = w_init(shape=([inp_shape[-1],inp_shape[-1]],self.units),) trainable=True) #shape : n*n
-        self.Ua = tf.Variable (initial_value = w_init(shape=([inp_shape[-1],inp_shape[-1]],self.units),)trainable=True) #shape : n*2n
+        # initialize the weights matrices (state of the layer)
+        inp_shape = dec_hdim
+        # Matrices Ua and Wa -> Gaussian distribution of mean 0 and variance of 0.001**2
+        initializer = tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.001**2)
+        self.Wa = initializer(shape=([inp_shape[-1],inp_shape[-1]],self.units), trainable=True) #shape : n*n
+        self.Ua = initializer(shape=([inp_shape[-1],inp_shape[-1]],self.units), trainable=True) #shape : n*2n
+        
+        # Va is initialized at zero
+        v_init = tf.keras.initializers.Zeros() 
+        self.Va = v_init(shape=(inp_shape[-1], self.units), trainable=True) #shape : n, with n : number of hidden states 
         
     def call (self, outp_enc, hidden_dec ) : # hidden state of the decoder, outp_enc = outputs of the encoder
         #We compute the attention : 
-        a  = self.Va.T*tanh(self.Wa*hidden_dec[-1]+self.Ua*outp_enc )  #hidden_dec[-1] : decoder hidden state of i-1,
-        alpha = tf.nn.softmax(a) #We normalize the output probabilities
+        a  = self.Va.T * tanh(self.Wa*hidden_dec[-1]+self.Ua*outp_enc )  # hidden_dec[-1] : decoder hidden state of i-1,
+        alpha = tf.nn.softmax(a) # We normalize the output probabilities
          
-        c = np.sum(alpha*hidden_dec) #context 
+        c = np.sum(alpha*hidden_dec) # context 
         return c
  
