ee4cba0838c688783c5be9afb6723e6b30bc17ac
Koloina RANDRIANAVONY <91273793+ko-randria@users.noreply.github.com>
2022-12-06 23:55:26 +0100

    Update Attention_S2S.py

--- Attention_S2S.py
+++ Attention_S2S.py
@@ -1,22 +1,27 @@
-from tensorflow.keras.layers import Layer
-from tensorflow.keras.activations import *
 import tensorflow as tf
+from tensorflow import keras
+from keras.layers import Layer
+from keras.activations import *
 import numpy as np 
 
-class attention_trad (Layer) :
-    def __init__(self, enc_outdim, dec_hdim) :    #We need the hidden satate of the decoder and the encoder 
+class Attention_trad (Layer) :
+    def __init__(self, dec_hdim) :    #We need the hidden satate of the decoder and the encoder 
         #We apply a linear transformation 
         super(attention_trad, self).__init__()
+        self.inp_shape = dec_hdim
         
     def build (self, inp_shape = dec_hdim) : #State of the layer (weights)
-        w_init = tf.keras.initializers.Zeros() 
+        w_init = tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.001**2)
         #Weight matrices 
-        self.Va = tf.Variable (initial_value=w_init(shape=(inp_shape[-1], self.units),) trainable=True)#shape : n, with n : number of hidden states 
-        self.Wa = tf.Variable (initial_value = w_init(shape=(inp_shape[-1]*inp_shape[-1],self.units),) trainable=True) #shape : n*n
-        self.Ua = tf.Variable (initial_value = w_init(shape=(inp_shape[-1]*2inp_shape[-1],self.units),)trainable=True) #shape : n*2n
+        self.Wa = tf.Variable(initial_value = w_init(shape=(self.inp_shape[-1]*self.inp_shape[-1],self.units),), trainable=True) #shape : n*n
+        self.Ua = tf.Variable(initial_value = w_init(shape=(self.inp_shape[-1]*2*self.inp_shape[-1],self.units),),trainable=True) #shape : n*2n
+        # Va is initialized at zero
+        v_init = tf.keras.initializers.Zeros() 
+        self.Va = tf.Variable(initial_value=v_init(shape=(self.inp_shape[-1], self.units),), trainable=True)#shape : n, with n : number of hidden states
+        
     def call (self, outp_enc, hidden_dec ) : #hidden : hj, hidden state of the j th word (from the encoder), out_dec = hidden state decoder 
         #We compute the attention : 
-        a = self.Va.T*tanh(self.Wa*hidden_dec[-1]+self.Ua*outp_enc)  #hidden_dec[-1] : decoder hidden state of i-1, (one value)
+        a = self.Va.T * tanh(self.Wa *hidden_dec[-1] + self.Ua * outp_enc)  #hidden_dec[-1] : decoder hidden state of i-1, (one value)
         alpha = tf.nn.softmax(a) #We normalize the output probabilities
          
         c = np.sum(alpha*hidden_dec) #context 
