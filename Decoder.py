6f50e618ea1194ee0acbdf597f0a1438505eff49
Koloina RANDRIANAVONY <91273793+ko-randria@users.noreply.github.com>
2022-12-22 18:29:20 +0100

    Update Decoder.py

--- Decoder.py
+++ Decoder.py
@@ -3,7 +3,6 @@ from tensorflow import keras
 from keras.layers import Embedding, Dropout, GRU, Dense
 from keras.layers import Layer
 from keras.activations import *
-import numpy as np 
 from Attention_S2S import attention
 
 class Decoder (Layer) :
@@ -12,35 +11,27 @@ class Decoder (Layer) :
         
         super(Decoder, self).__init__()
         self.attention = attention 
-        self.embedding = Embedding(size_emb, size_emb) #Embedding matrix of the target word
+        self.embedding = Embedding(size_emb, size_emb) # Embedding matrix of the target word
+
         w_init = tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.001**2)
-        self.rnn = GRU (dec_dimh, kernel_initializer = w_init ) 
+        self.rnn = GRU (1000, kernel_initializer = w_init, return_sequences=False, input_shape = (1,1000))
         self.dropout = Dropout(t_drop)
         self.max_hid_lay = max_hid_lay
         self.enc_dimh = enc_dimh 
         self.size_emb = size_emb
-        self.dense = Dense (size_out,  activation = None )  #Dense must have the lenght of the target as the size output 
+        self.dense = Dense (size_out,  activation = None )  # Dense must have the lenght of the target as the size output 
 
-    def call (self, entr, hidden, outp_enc, iterat) :
-
+    def call (self, entr, outp_enc, hidden) :
         emb =  self.embedding(entr)
 
-        emb_r = tf.expand_dims(emb,axis=0) #emb_r : [1,len(entr),size_emb]
-        emb_r = self.dropout (emb_r) #Dropout step
-        attr = self.attention.call( hidden,outp_enc) #We compute the attention between this two terms 
-        
-        #The decoder given the 
-        emb = tf.expand_dims(emb_r,axis=0)
-        hid_dec = self.rnn(emb, hidden) 
-        
-        #We concatenate the three elements : attention,the output RNN decoder, the embedded world
-        predict = self.dense (tf.concat ([hid_dec, emb_r, attr],1)) #tf.concat : [len(entr), 2*enc_dimh+size_emb]
+        emb_r = tf.expand_dims(emb,axis=0) # emb_r : [1,len(entr),size_emb]
+        emb_r = self.dropout (emb_r) # Dropout step
+        attr = self.attention.call(outp_enc, hidden) # We compute the attention between this two terms 
 
-        #ti = np.max(t[:, 2*iterat-1:2*iterat])
-        #print(ti)
-        #prediction = np.exp(tf.transpose(entr)*t)  
-        
-        #prediction = tf.math.reduce_max(predict)
+        hid_dec = self.rnn (emb_r )
 
-        return predict, hidden #We send the predicted word yi, and new values of the hidden states 
+        predict = self.dense (tf.concat ([hid_dec, tf.expand_dims(tf.squeeze(emb_r), axis=0), attr],1)) 
+
+        return [predict, hid_dec]
+
     
