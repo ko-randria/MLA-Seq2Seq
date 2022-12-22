1920b6b0e4e50797376d37ac5a609852919b76c0
Koloina RANDRIANAVONY <91273793+ko-randria@users.noreply.github.com>
2022-12-22 18:26:33 +0100

    Update Encoder.py

--- Encoder.py
+++ Encoder.py
@@ -10,17 +10,20 @@ class Encoder (Layer):
          super(Encoder, self)._init_()
          #ri :output of the reset gate
          self.emb = Embedding(intdim, embdim) #size of the sequence, and dimension of the embedding
-         self.RNN =Bidirectional(GRU (enc_dimh,return_state = True )) #Choose the GRU because he returns the hidden states and the outputs 
-         self.drop = Dropout(t_drop)
-         self.lin = Dense (dec_dim_h, activation = None ) #output size,  because Multilayer perceptron 
+         self.RNN =Bidirectional(GRU (enc_dimh, return_sequences=False, return_state = True ))  #Choose the GRU because he returns the hidden states and the outputs
+         self.drop = Dropout(t_drop) 
+         self.lin = Dense(dec_dim_h, activation = None ) # Because Multilayer perceptron
              
-     def call (self, seq) :
+    def call (self, seq) :
          emb_mod  = self.emb(seq) #Embedding step we give to our data different dimension 
+            
          emb_mod = self.drop(emb_mod) #Dropout, we want to avoid the overfitting 
-         outp, hid_state_f, hid_state_b  = self.RNN(emb_mod)          
-     
-         #We apply the activation function the last forward and the last backward state 
-         #Each of backward and forward state have their own weights matrices 
-         hidden = tanh(self.lin(tf.concat((hid_state_f, hid_state_b), -1)))# tf.concat convert to one dimension, self.lin apply a linear transformation to the data                  
-         return outp, hidden  #We return the outputs and the hidden states 
+
+         emb_mod = tf.expand_dims(emb_mod, axis=0)
+         outp, hidden, hd= self.RNN(emb_mod)   #hid_state, hd    
+         # We apply the activation function the last forward and the last backward state 
+         # Each of backward and forward state have their own weights matrices
+         hidden = tanh(self.lin(tf.concat((hidden, hd),-1)))# tf.concat convert to one dimension, self.lin apply a linear transformation to the data                  
+         
+         return [outp, hidden]  # We return the outputs and the hidden states 
 
